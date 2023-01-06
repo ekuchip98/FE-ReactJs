fc198f27a8e3a69fd87c2498407535bd867407d0
thunguyen <thu.nguyen@tomosia.com>
2023-01-06 14:29:03 +0700

    change language

--- src/containers/App.js
+++ src/containers/App.js
@@ -16,9 +16,8 @@ import Home from "../routes/Home";
 import Login from "./Auth/Login";
 import Header from "./Header/Header";
 import System from "../routes/System";
-
+import HomePage from "./Homepage/HomePage";
 import { CustomToastCloseButton } from "../components/CustomToast";
-import ConfirmModal from "../components/ConfirmModal";
 
 class App extends Component {
   handlePersistorState = () => {
@@ -44,7 +43,6 @@ class App extends Component {
       <Fragment>
         <Router history={history}>
           <div className="main-container">
-            <ConfirmModal />
             {this.props.isLoggedIn && <Header />}
 
             <span className="content-container">
@@ -58,6 +56,7 @@ class App extends Component {
                   path={path.SYSTEM}
                   component={userIsAuthenticated(System)}
                 />
+                <Route path={path.HOMEPAGE} component={HomePage} />
               </Switch>
             </span>
 
